9d9e5aed7aee567e215e8c797a5fb876ba7a9b6d
Wakestand <wakeisle9933@gmail.com>
2021-11-10 00:36:43 +0900

    신규 추가

---
+++ src/main/java/com/google/googleanalytics/GoogleAnalyticsApplication.java
@@ -0,0 +1,13 @@
+package com.google.googleanalytics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoogleAnalyticsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GoogleAnalyticsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/google/googleanalytics/GoogleAnalyticsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.google.googleanalytics;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoogleAnalyticsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
